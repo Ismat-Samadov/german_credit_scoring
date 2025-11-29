1f2f08c586d1cd7fb7d1848c30501adb3c8a8057
ismat-samadov <ismetsemedov@gmail.com>
2025-11-29 17:17:02 +0400

    init

--- AbstractClass/NodeList.java
+++
@@ -1,9 +0,0 @@
-package AbstractClass;
-
-public interface NodeList {
-    ListItem getRoot();
-    boolean addItem(ListItem newItem);
-    boolean removeItem(ListItem item);
-    void traverse(ListItem root);
-}
-
--- SimpleInterface/ISaveable.java
+++
@@ -1,8 +0,0 @@
-package SimpleInterface;
-import java.util.List;
-
-public interface ISaveable {
-    List<String> write();
-    void read(List<String> savedValues);
-}
-
